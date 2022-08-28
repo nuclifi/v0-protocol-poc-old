9e68973731e4b7864d5358f9ad235c7ad8d9f5bb
Yash Agrawal <yashagrawal2299@gmail.com>
2022-08-28 09:58:18 +0530

    feat(contracts): update strategy factory

--- contracts/test/strategy/MockStakingStrategyFactory.sol
+++ contracts/test/strategy/MockStakingStrategyFactory.sol
@@ -2,48 +2,84 @@
 
 pragma solidity ^0.8.0;
 
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 import {Errors} from "../../libraries/Errors.sol";
 import {MockStakingStrategy} from "./MockStakingStrategy.sol";
+import {INuclifiStrategy} from "../../interfaces/INuclifiStrategy.sol";
 import {INuclifiCertificate} from "../../interfaces/INuclifiCertificate.sol";
+import {INuclifiStrategyFactory} from "../../interfaces/INuclifiStrategyFactory.sol";
 
-contract MockStakingStrategyFactory is Ownable, ReentrancyGuard {
+contract MockStakingStrategyFactory is
+    INuclifiStrategyFactory,
+    Ownable,
+    ReentrancyGuard
+{
     using Address for address;
 
-    address public controller;
-    address public stakingProgram;
-    address public nuclifiCertificate;
+    address public stakingProgramAddress;
+    address public nuclifiControllerAddress;
+    address public nuclifiCertificateAddress;
+
+    event StakingProgramAddressChanged(address stakingProgram_);
+    event NuclifiControllerAddressChanged(address nuclifiControllerAddress_);
+    event NuclifiCertificateAddressChanged(address nuclifiCertificateAddress_);
 
     function setAddresses(
-        address controller_,
-        address stakingProgram_,
-        address nuclifiCertificate_
-    ) external onlyOwner {
-        controller = controller_;
-        stakingProgram = stakingProgram_;
-        nuclifiCertificate = nuclifiCertificate_;
+        address stakingProgramAddress_,
+        address nuclifiControllerAddress_,
+        address nuclifiCertificateAddress_
+    ) external nonReentrant onlyOwner {
+        require(
+            stakingProgramAddress_.isContract(),
+            Errors.ADDRESS_NOT_CONTRACT
+        );
+        require(
+            nuclifiControllerAddress_.isContract(),
+            Errors.ADDRESS_NOT_CONTRACT
+        );
+        require(
+            nuclifiCertificateAddress_.isContract(),
+            Errors.ADDRESS_NOT_CONTRACT
+        );
+
+        emit StakingProgramAddressChanged(stakingProgramAddress_);
+        stakingProgramAddress = stakingProgramAddress_;
+
+        emit NuclifiControllerAddressChanged(nuclifiControllerAddress_);
+        nuclifiControllerAddress = nuclifiControllerAddress;
+
+        emit NuclifiCertificateAddressChanged(nuclifiCertificateAddress_);
+        nuclifiCertificateAddress = nuclifiCertificateAddress_;
+
+        renounceOwnership();
     }
 
-    function generate(uint256 tokenId) external returns (address) {
-        require(msg.sender == controller, "Invalid");
+    function deployStrategy()
+        external
+        override
+        nonReentrant
+        returns (address)
+    {
+        requireCallerIsNuclifiControllerAddress();
 
-        address strategy = address(
+        INuclifiStrategy strategy = INuclifiStrategy(
             new MockStakingStrategy(
-                controller,
-                stakingProgram,
-                nuclifiCertificate
+                stakingProgramAddress,
+                nuclifiControllerAddress,
+                nuclifiCertificateAddress
             )
         );
 
-        MockStakingStrategy(strategy).setCertificateId(tokenId);
+        return address(strategy);
+    }
 
-        return strategy;
+    function requireCallerIsNuclifiControllerAddress() internal view {
+        require(
+            _msgSender() == nuclifiControllerAddress,
+            Errors.SENDER_NOT_NUCLIFI_CONTROLLER
+        );
     }
 }
